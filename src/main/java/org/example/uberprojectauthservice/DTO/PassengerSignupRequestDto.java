50355b0748aca6d44798a64de67fd4668a8e7761
Ishlok Dwivedi <ishlokdwivedi1@gmail.com>
2025-10-01 12:47:32 +0530

    Added First plain API with Dtos

---
+++ src/main/java/org/example/uberprojectauthservice/DTO/PassengerSignupRequestDto.java
@@ -0,0 +1,22 @@
+package org.example.uberprojectauthservice.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerSignupRequestDto {
+
+   private String email;
+
+   private String password;
+
+   private String phoneNumber;
+
+    private String name;
+
+
+}
+
